d4bdddb40d501f84753d7cf5721a06e16e7cb4fe
dashluu <dashluu9121997@gmail.com>
2023-07-14 14:56:43 -0700

    Added a body node.

--- src/main/java/ast/ElseASTNode.java
+++ src/main/java/ast/ElseASTNode.java
@@ -2,9 +2,19 @@ package ast;
 
 import toks.Tok;
 
-public class ElseASTNode extends KnaryASTNode {
+public class ElseASTNode extends ASTNode {
+    private ScopeASTNode bodyNode;
+
     public ElseASTNode(Tok tok) {
         super(tok, ASTNodeType.ELSE, null);
+    }
+
+    public ScopeASTNode getBodyNode() {
+        return bodyNode;
+    }
+
+    public void setBodyNode(ScopeASTNode bodyNode) {
+        this.bodyNode = bodyNode;
     }
 
     @Override
