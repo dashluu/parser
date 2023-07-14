3e359d9d6c82b48162b0a9958da50504d4b0b949
dashluu <dashluu9121997@gmail.com>
2023-07-14 09:30:39 -0700

    Removed redundant TYPE_CONV and added a comment.

--- src/main/java/parsers/utils/SyntaxTag.java
+++ src/main/java/parsers/utils/SyntaxTag.java
@@ -1,7 +1,8 @@
 package parsers.utils;
 
+// A tag to assign to a SyntaxInfo object used in parsing
 public enum SyntaxTag {
-    PREFIX, POSTFIX, INFIX, LPAREN, RPAREN, TYPE_CONV, ID, LITERAL,
+    PREFIX, POSTFIX, INFIX, LPAREN, RPAREN, ID, LITERAL,
     VAR_DECL, CONST_DECL, TYPE_ID, ASGNMT,
     FUN_CALL, COMMA,
     FUN_DEF, PARAM,
