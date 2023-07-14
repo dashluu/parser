8ffa95cb0158791d9dd96a3053bfc7282845e247
dashluu <dashluu9121997@gmail.com>
2023-07-14 13:34:10 -0700

    Added comments and documentation.

--- src/main/java/parsers/utils/SyntaxBuff.java
+++ src/main/java/parsers/utils/SyntaxBuff.java
@@ -8,20 +8,31 @@ public class SyntaxBuff implements Iterable<SyntaxInfo> {
     // Delegation to the List interface
     private final List<SyntaxInfo> buff = new ArrayList<>();
     private int cursor;
+    // A sentinel for the buffer so the buffer will not be underflowed
     private static final SyntaxInfo SENTINEL = new SyntaxInfo(null, SyntaxTag.END);
 
     public SyntaxBuff() {
+        // Add the sentinel before pushing anything onto the stack
         buff.add(SENTINEL);
         cursor = 0;
     }
 
+    /**
+     * Adds a SyntaxInfo object to the buffer.
+     *
+     * @param syntaxInfo the SyntaxInfo object to be added.
+     */
     public void add(SyntaxInfo syntaxInfo) {
         // Overwrite the sentinel with new element and add the sentinel to keep it at the tail
         buff.set(buff.size() - 1, syntaxInfo);
         buff.add(SENTINEL);
-        return;
     }
 
+    /**
+     * Increments the cursor and returns the next SyntaxInfo object at the new cursor position.
+     *
+     * @return a valid SyntaxInfo object if it is within bounds, otherwise, return the sentinel.
+     */
     public SyntaxInfo forward() {
         if (cursor == buff.size() - 1) {
             // The last one is always the sentinel
@@ -32,6 +43,11 @@ public class SyntaxBuff implements Iterable<SyntaxInfo> {
         return syntaxInfo;
     }
 
+    /**
+     * Decrements the cursor and returns the next SyntaxInfo object at the new cursor position.
+     *
+     * @return a valid SyntaxInfo object if it is within bounds, otherwise, return the sentinel.
+     */
     public SyntaxInfo backward() {
         if (cursor == 0) {
             return SENTINEL;
@@ -41,22 +57,43 @@ public class SyntaxBuff implements Iterable<SyntaxInfo> {
         return syntaxInfo;
     }
 
+    /**
+     * Peeks the SyntaxInfo object at the current cursor position.
+     *
+     * @return a SyntaxInfo object.
+     */
     public SyntaxInfo peek() {
         return buff.get(cursor);
     }
 
+    /**
+     * Moves the cursor to the front of the buffer.
+     */
     public void toFront() {
         cursor = 0;
     }
 
+    /**
+     * Checks if the cursor is at the front of the buffer.
+     *
+     * @return true if the cursor is at the front of the buffer and false otherwise.
+     */
     public boolean atFront() {
         return cursor == 0;
     }
 
+    /**
+     * Moves the cursor to the back of the buffer.
+     */
     public void toBack() {
         cursor = buff.size() - 1;
     }
 
+    /**
+     * Checks if the cursor is at the back of the buffer.
+     *
+     * @return true if the cursor is at the back of the buffer and false otherwise.
+     */
     public boolean atBack() {
         return cursor == buff.size() - 1;
     }
